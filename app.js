4adfa5cba58c75e7cb2a9c9f140f2f04a5582fb0
Cesar Hdez <alejandroho23@hotmail.com>
2021-05-29 07:36:54 -0500

    especificaciones

--- app.js
+++ app.js
@@ -5,15 +5,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+var PORT = process.env.PORT || 5000;
 require('dotenv/config');
 //Importamos los comandos de la api
 const postsRoute = require('./routes');
 app.use(express.json());
 //Aqui llamamos a los que usaremos.
 app.use('/', postsRoute);
-app.get('/', (req, res) => {
-    res.send('Test de la API!');
-});
+
 //Conexion a la BD
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
     console.log('Conectado a la base!'));
